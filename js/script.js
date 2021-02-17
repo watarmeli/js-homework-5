495e4a5b5b434f3d327474b2cf9628ec139576f2
watarmeli <watar@ukr.net>
2021-02-17 19:44:30 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,29 @@
+class Car {
+  // Пиши код ниже этой строки
+  static MAX_PRICE = 50000;
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  set price(newPrice) {
+    if (newPrice <= Car.MAX_PRICE) {
+      this.#price = newPrice;
+    };
+  }
+  // Пиши код выше этой строки
+}
+
+const audi = new Car({price: 35000});
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
