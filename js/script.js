d3d8078ea22cd127ad9e712c498eaa4d4b636baa
watarmeli <watar@ukr.net>
2021-02-17 22:10:34 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,40 @@
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+
+class Admin extends User {
+  // Пиши код ниже этой строки
+  accessLevel;
+  
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
+  
+  constructor({email, accessLevel}) {
+    super(email);
+    this.AccessLevel;   
+  }
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
