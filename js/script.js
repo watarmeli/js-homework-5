7fb7eeb7ace1a21051a7a0a32cca2e1a5b4c6b2c
watarmeli <watar@ukr.net>
2021-02-17 22:19:23 +0200

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,57 @@
+class User {
+  email;
+
+  constructor(email) {
+    this.email = email;
+  }
+
+  get email() {
+    return this.email;
+  }
+
+  set email(newEmail) {
+    this.email = newEmail;
+  }
+}
+class Admin extends User {
+  // Пиши код ниже этой строки
+
+  static AccessLevel = {
+    BASIC: 'basic',
+    SUPERUSER: 'superuser'
+  };
+
+  accessLevel;
+  
+  blacklistedEmails = [];
+  
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+  
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+  
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
+  
+  
+
+  // Пиши код выше этой строки
+}
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER
+});
+
+console.log(mango.email); // mango@mail.com
+console.log(mango.accessLevel); // superuser
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // 'poly@mail.com'
+console.log(mango.isBlacklisted('mango@mail.com')); //  false
+console.log(mango.isBlacklisted('poly@mail.com')); // true 
