63dacb027f0a1372a1b40500aa2eda2783e14c63
watarmeli <watar@ukr.net>
2021-02-17 16:23:44 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 class Car {
-  	brand;
- 	model;
-  	price;
-  
-    constructor({ brand, model, price }) {
-        this.brand = brand;
-        this.model = model;
-        this.price = price;
+  constructor({ brand, model, price }) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
     };
 };
